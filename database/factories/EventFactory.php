ebf65a60b800ca3762a47f41e164e763f561c57b
agitnaeta <agitnaeta@gmail.com>
2024-10-27 09:45:49 +0700

    Update Factory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,12 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Event;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
- */
 class EventFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +22,17 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
+        $startDate = $this->faker->dateTimeBetween('-1 month', '+1 month');
+        $endDate = (clone $startDate)->modify('+'.rand(1, 3).' hours'); // End date 1 to 3 hours after start date
+
         return [
-            //
+            'title' => $this->faker->sentence(5),               // Generates a random title
+            'description' => $this->faker->sentence(200),           // Generates a random paragraph for description
+            'start_date' => $startDate,                         // Random start date
+            'end_date' => $endDate,                             // End date after start date
+            'location' => $this->faker->city,                   // Generates a random city as location
+            'created_at' => now(),                              // Current date and time
+            'updated_at' => now(),                              // Current date and time
         ];
     }
 }
